1ea8c384e9cdd53538af046c6d1433d2f73024ed
Manick Vel <mkumaravel@msystechnologies.com>
2021-05-25 18:34:28 +0530

    Creating file resource

---
+++ cron_cookbook_sample/recipes/new1.rb
@@ -0,0 +1,5 @@
+
+
+file '/home/ad.msystechnologies.com/mkumaravel/git/chef_learnings/cron_cookbook_sample/recipes/x.txt' do
+  content Time.now.to_s
+end
--- cron_cookbook_sample/recipes/sample1.sh
+++
@@ -1,6 +0,0 @@
-touch /home/ad.msystechnologies.com/mkumaravel/git/chef_learnings/cron_cookbook_sample/recipes/file1.txt
-
-now=$(date)
-
-echo "$now"
-echo "Current date: $now"
